74b82ea257115a7a1e552355821366c0577753db
Adrian Ilker Berruga Garcia <ai.garcia@student.maastrichtuniversity.nl>
2024-06-09 01:39:12 +0200

    Add delete instance

--- src/test/java/alerts/DecoratorTest.java
+++ src/test/java/alerts/DecoratorTest.java
@@ -1,12 +1,15 @@
 package alerts;
 
+import com.alerts.alert_types.Alert;
+import com.alerts.decorators.RepeatedAlertDecorator;
 import org.junit.jupiter.api.Test;
 
 public class DecoratorTest {
 
     @Test
     public void testRepeatDecorator() {
-
+        Alert alert = new Alert("1", "BloodSaturation", 100);
+        Alert test = new RepeatedAlertDecorator()
     }
 
     @Test
